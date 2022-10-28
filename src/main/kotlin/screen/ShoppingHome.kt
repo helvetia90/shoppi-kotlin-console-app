eb92c54f154a0dc73f4f6e466617ed88f24d0cc2
voemvnt <voemvnt@gmail.com>
2022-10-28 22:18:59 +0900

    살습 3-3. Product 객체 추가 및 ProductList 화면이 상품 출력 담당

--- src/main/kotlin/screen/ShoppingHome.kt
+++ src/main/kotlin/screen/ShoppingHome.kt
@@ -21,21 +21,7 @@ class ShoppingHome {
     }
 
     private fun showCategories() {
-        val categories = arrayOf("패션", "전자기기", "반려동물용품")
-        for (category in categories) {
-            println(category)
-        }
-        println("=> 장바구니로 이동하시려면 #을 입력해주세요")
-
-        var selectedCategory = readLine()
-        while (selectedCategory.isNullOrBlank()) {
-            println("값을 입력해주세요")
-            selectedCategory = readLine()
-        }
-        if (selectedCategory == "#") {
-            // 1.test
-        } else {
-            //2.test
-        }
+        val shoppingCategory = ShoppingCategory()
+        shoppingCategory.showCategories()
     }
 }

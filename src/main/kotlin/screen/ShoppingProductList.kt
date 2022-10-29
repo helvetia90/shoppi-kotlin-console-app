589e1e68e049bda509ec78484bf150a7fad462e3
voemvnt <voemvnt@gmail.com>
2022-10-29 15:34:51 +0900

    살습 5-2. ShoppingCart 이전화면, 이동 기능 추가

--- src/main/kotlin/screen/ShoppingProductList.kt
+++ src/main/kotlin/screen/ShoppingProductList.kt
@@ -6,7 +6,7 @@ import data.Product
 import extension.getNotEmptyInt
 import extension.getNotEmptyString
 
-class ShoppingProductList : Screen() {
+class ShoppingProductList(private val selectedCategory: String) : Screen() {
     private val products = arrayOf(
         Product("패션", "겨울 패딩"),
         Product("패션","겨울 바지"),
@@ -23,7 +23,7 @@ class ShoppingProductList : Screen() {
             product -> product.categoryLabel
     }
 
-    fun showProducts(selectedCategory: String) {
+    fun showProducts() {
         ScreenStack.push(this)
         val categoryProducts = categories[selectedCategory]
         if(!categoryProducts.isNullOrEmpty()) {
@@ -43,13 +43,13 @@ class ShoppingProductList : Screen() {
             categoryProducts.forEachIndexed { index, product ->
                 println("${index}. ${product.name}")
             }
-            showCartOption(categoryProducts, selectedCategory)
+            showCartOption(categoryProducts)
         } else {
             showEmptyProductMessage(selectedCategory)
         }
     }
 
-    private fun showCartOption(categoryProducts: List<Product>, selectedCategory: String) {
+    private fun showCartOption(categoryProducts: List<Product>) {
         println("""
             $LINE_DIVIDER
             장바구니에 담을 상품 번호를 선택해주세요.
@@ -66,10 +66,13 @@ class ShoppingProductList : Screen() {
                 val shoppingCart = ShoppingCart()
                 shoppingCart.showCartItems()
             } else if (answer == "*") {
-                showProducts(selectedCategory)
+                showProducts()
             } else {
                 // TODO
             }
+        } ?: kotlin.run {
+            println("$selectedIndex 번은 목록에 없는 상품 번호 입니다. 다시 입력 해주세요.")
+            showProducts()
         }
     }
 
